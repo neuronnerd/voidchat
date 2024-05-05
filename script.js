338cdf24aaa82c4653889dc6c57404bfa9ed6b2c
rulercosta <rulercosta.mail@gmail.com>
2024-05-06 00:35:33 +0530

    Modified DOM output in script.js

--- script.js
+++ script.js
@@ -7,17 +7,15 @@ function randomEmoji() {
 const emoji = randomEmoji();
 const name = prompt("What's your name?");
 
-// Generate random chat hash if needed
 if (!location.hash) {
   location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
 }
 const chatHash = location.hash.substring(1);
 
-// TODO: Replace with your own channel ID
+// Generic 3rd party signalling server
 const drone = new ScaleDrone('yiS12Ts5RdNhebyM');
-// Scaledrone room name needs to be prefixed with 'observable-'
+// roomName prefixed with 'observable-'
 const roomName = 'observable-' + chatHash;
-// Scaledrone room used for signaling
 let room;
 
 const configuration = {
@@ -30,7 +28,6 @@ let pc;
 // RTCDataChannel
 let dataChannel;
 
-// Wait for Scaledrone signalling server to connect
 drone.on('open', error => {
   if (error) {
     return console.error(error);
@@ -42,19 +39,15 @@ drone.on('open', error => {
     }
     console.log('Connected to signaling server');
   });
-  // We're connected to the room and received an array of 'members'
-  // connected to the room (including us). Signaling server is ready.
   room.on('members', members => {
     if (members.length >= 3) {
       return alert('The room is full');
     }
-    // If we are the second user to connect to the room we will be creating the offer
     const isOfferer = members.length === 2;
     startWebRTC(isOfferer);
   });
 });
 
-// Send signaling data via Scaledrone
 function sendSignalingMessage(message) {
   drone.publish({
     room: roomName,
@@ -66,8 +59,6 @@ function startWebRTC(isOfferer) {
   console.log('Starting WebRTC in as', isOfferer ? 'offerer' : 'waiter');
   pc = new RTCPeerConnection(configuration);
 
-  // 'onicecandidate' notifies us whenever an ICE agent needs to deliver a
-  // message to the other peer through the signaling server
   pc.onicecandidate = event => {
     if (event.candidate) {
       sendSignalingMessage({'candidate': event.candidate});
@@ -76,14 +67,12 @@ function startWebRTC(isOfferer) {
 
 
   if (isOfferer) {
-    // If user is offerer let them create a negotiation offer and set up the data channel
     pc.onnegotiationneeded = () => {
       pc.createOffer(localDescCreated, error => console.error(error));
     }
     dataChannel = pc.createDataChannel('chat');
     setupDataChannel();
   } else {
-    // If user is not the offerer let wait for a data channel
     pc.ondatachannel = event => {
       dataChannel = event.channel;
       setupDataChannel();
@@ -94,24 +83,20 @@ function startWebRTC(isOfferer) {
 }
 
 function startListentingToSignals() {
-  // Listen to signaling data from Scaledrone
   room.on('data', (message, client) => {
     // Message was sent by us
     if (client.id === drone.clientId) {
       return;
     }
     if (message.sdp) {
-      // This is called after receiving an offer or answer from another peer
       pc.setRemoteDescription(new RTCSessionDescription(message.sdp), () => {
         console.log('pc.remoteDescription.type', pc.remoteDescription.type);
-        // When receiving an offer lets answer it
         if (pc.remoteDescription.type === 'offer') {
           console.log('Answering offer');
           pc.createAnswer(localDescCreated, error => console.error(error));
         }
       }, error => console.error(error));
     } else if (message.candidate) {
-      // Add the new ICE candidate to our connections remote description
       pc.addIceCandidate(new RTCIceCandidate(message.candidate));
     }
   });
@@ -125,7 +110,6 @@ function localDescCreated(desc) {
   );
 }
 
-// Hook up data channel event handlers
 function setupDataChannel() {
   checkDataChannelState();
   dataChannel.onopen = checkDataChannelState;
@@ -159,7 +143,6 @@ function insertMessageToDOM(options, isFromMe) {
   const messagesEl = document.querySelector('.messages');
   messagesEl.appendChild(clone);
 
-  // Scroll to bottom
   messagesEl.scrollTop = messagesEl.scrollHeight - messagesEl.clientHeight;
 }
 
@@ -180,4 +163,4 @@ form.addEventListener('submit', () => {
   insertMessageToDOM(data, true);
 });
 
-insertMessageToDOM({content: '[ROOM LINK]\n' + location.href});
+insertMessageToDOM({content: location.href});
